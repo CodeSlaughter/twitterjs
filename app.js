b336a0e845e15348e426f5602139de87d6f3c8bf
Iam Slaughter <iamslaughter@Iams-MacBook-Pro.local>
2017-09-19 15:45:41 -0400

    finished

--- app.js
+++ app.js
@@ -2,42 +2,35 @@ const express = require( 'express' );
 const routes = require('./routes');
 const app = express();
 const nunjucks = require('nunjucks');
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}, {name: 'Jamie'}, {name: 'Patrick'}];
-const places = [{location: 'Chicago'}, {location: 'New Jersey(trashcan)'}, {location: 'New York'}];
-// var locals = {
-//     title: 'An Example',
-//     people: [
-//         { name: 'Gandalf'},
-//         { name: 'Frodo' },
-//         { name: 'Hermione'}
-//     ]
-// };
-// nunjucks.render('index.html', locals, function (err, output) {
-    //     console.log(output);
-    // });
-    // app.set("viewEngine", "html");
-    // app.engine("html", nunjucks.render);
-    // nunjucks.configure('views', {noCache: true});
-
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+const socketio = require('socket.io');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views');
 
 
-app.use(routes, function (req, res, next) {
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-    console.log(res, req);
-    next();
-});
+// app.use(routes, function (req, res, next) {
+//     // do your logging here
+//     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+//     console.log(res, req);
+//     next();
+// });
 
+// app.use( '/', routes(io) );
 // app.get('/',function(req, res){
 //     console.log("get running");
 //     //res.render(__dirname + '/views/index.html');  
 //     res.render( 'index', {title: 'Hall of Fame', people: people, places: places} );
 // });
 
-app.listen(3000, function(){
-    console.log("Listening on port 3000");
-});
+// app.listen(3000, function(){
+//     console.log("Listening on port 3000");
+// });
+
+const server = app.listen(3000);
+const io = socketio.listen(server);
+
+app.use( '/', routes(io) );
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+module.exports = function (io) {
 router.use(express.static('public'));
 
 router.get('/', function(req, res) {
     let tweets = tweetBank.list();
-    res.render ('index', {tweets:tweets});
+    res.render ('index', {tweets:tweets, showForm: true});
 });
 
 router.get('/users/:name', function(req, res) {
     var person = req.params.name;
     var tweets = tweetBank.find( {name: person} );
-    res.render( 'index', { tweets: tweets }, {showForm: true} );
+    res.render( 'index', { tweets: tweets, showForm: true, input: person},);
   });
 
 router.get('/tweets/:id', function(req, res) {
     var num = (req.params.id)*1;
     var tweets2 = tweetBank.find( {id: num} );
-    res.render( 'index', { tweets: tweets2 } );
+    res.render( 'index', { tweets: tweets2, showForm: false } );
 });
 
-module.exports = router;
+router.post('/tweets', urlencodedParser, function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('/');
+  });
+
+  return router;
+};
